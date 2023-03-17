8c38c52a9a54c382ebaaddca2c312c34c07fb1ac
Akeem McKenzie <akeem.mckenzie@ondefend.com>
2023-03-17 15:46:35 -0400

    static files

--- arithmetic_calculator/settings.py
+++ arithmetic_calculator/settings.py
@@ -169,7 +169,7 @@ AUTH_USER_MODEL = 'calculator.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
